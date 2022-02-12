f4d894dab4be034e267fbaef555a66ce9dce88d7
Restyled.io <commits@restyled.io>
2022-02-12 14:01:54 +0000

    Restyled by whitespace

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -1,29 +1,29 @@
-<?php 
+<?php
 
 namespace App\Entities;
 
 class User extends \CodeIgniter\Entity\Entity
-{ 
+{
     public function verifyPassword($password)
-    { 
-        #this the method we would to call on the user object 
-        #hence placing it on the User entity class 
-        
+    {
+        #this the method we would to call on the user object
+        #hence placing it on the User entity class
+
         return password_verify($password, $this->password_hash);
     }
 
     public function startActivation()
-    { 
+    {
         $this->token = bin2Hex(random_bytes(16));
 
         //assigning hash property to the current user object
         $this->activation_hash = hash_hmac('sha256', $this->token, $_ENV['HASH_SECRET_KEY']);
-        
+
     }
 
     public function activate()
-    { 
-        $this->is_active = true; 
+    {
+        $this->is_active = true;
         //set to null as we no longer need it
         $this->activation_hash = null;
     }
@@ -34,9 +34,9 @@ class User extends \CodeIgniter\Entity\Entity
     {
         //generate a random token by creating an object
         //of the token class
-        $token = new Token; 
+        $token = new Token;
 
-        $this->reset_token = $token->getValue(); 
+        $this->reset_token = $token->getValue();
 
         $this->reset_hash = $token->getHash();
 
@@ -46,7 +46,7 @@ class User extends \CodeIgniter\Entity\Entity
 
     public function completePasswordReset()
     {
-        $this->reset_hash = null; 
+        $this->reset_hash = null;
         $this->reset_expires_at = null;
     }
 
--- app/Views/Password/reset_success.php
+++ app/Views/Password/reset_success.php
@@ -1,9 +1,9 @@
-<?= $this->extend('layouts/default') ?> 
+<?= $this->extend('layouts/default') ?>
 
 <?= $this->section('title') ?> Password reset <?= $this->endSection() ?>
 
 <?= $this->section('content') ?>
-    <h1>Password reset</h1> 
+    <h1>Password reset</h1>
 
     <p> Password reset success </p>
 
